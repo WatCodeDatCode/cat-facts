77251bdea78a23a5557d1fe25dbb28c29e316302
WatCodeDatCode <arussell742@gmail.com>
2020-09-22 17:26:55 +0200

    Improve responsiveness and add comments to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,22 @@
+// IDs for sections
 const catFact = document.getElementById("cat-fact");
-const showNewFact = document.getElementById("new-fact");
+const showNewFact = document.getElementById("new-fact-button");
 const catPicture = document.getElementById("cat-picture");
+const showLoadingMessage = document.getElementById("loading-message");
 
-const catAPI = () => {
+// Function for consuming cat picture API and setting new src URL
+const randomCatPicture = () => {
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((json) => {
+      const picture = json[0].url;
+      catPicture.setAttribute("src", picture);
+      showLoadingMessage.style = "display:none";
+    });
+};
+
+// Function for displaying random cat fact from API
+const randomCatFact = () => {
   fetch(`https://catfact.ninja/fact`)
     .then((response) => response.json())
     .then((json) => {
@@ -18,20 +32,11 @@ const catAPI = () => {
     });
 };
 
-const randomCatPicture = () => {
-  fetch("https://api.thecatapi.com/v1/images/search")
-    .then((response) => response.json())
-    .then((json) => {
-      const picture = json[0].url;
-      catPicture.setAttribute("src", picture);
-    });
-};
-
-
+// When button clicked, recall functions for cat picture and cat fact
 showNewFact.addEventListener("click", () => {
-  catAPI();
   randomCatPicture();
+  randomCatFact();
 });
 
 randomCatPicture();
-catAPI();
+randomCatFact();
